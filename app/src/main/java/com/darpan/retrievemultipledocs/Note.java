d6402bc4ca16c88718aa3e416f6949ffa0d09849
darpanrajput <darpanrajput412@gmail.com>
2020-04-05 22:09:25 +0530

    Initial commit

---
+++ app/src/main/java/com/darpan/retrievemultipledocs/Note.java
@@ -0,0 +1,45 @@
+package com.darpan.retrievemultipledocs;
+
+import com.google.firebase.firestore.Exclude;
+
+public class Note {
+    private String documentId;
+    private String title;
+    private String description;
+    private int priority;
+
+    public Note() {
+        //public no-arg constructor needed
+    }
+
+    @Exclude
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
